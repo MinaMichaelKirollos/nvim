76ded3032176ba20cd212ff45e4d0906d615c410
Mina-Michael <minamichaelkirollos@gmail.com>
2024-03-30 19:00:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 💤 LazyVim
+
+Mina's extension to [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.mypy.with({
+          extra_args = function()
+            local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+            return { "--python-executable", virtual .. "/bin/python3" }
+          end,
+        }),
+        null_ls.builtins.formatting.isort,
+      },
+    })
+  end,
+}
